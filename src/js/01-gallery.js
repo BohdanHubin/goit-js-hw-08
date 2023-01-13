b9188151b0b81810d14674d66de94a53641ad4c3
BohdanHubin <111269462+BohdanHubin@users.noreply.github.com>
2023-01-13 21:04:08 +0200

    добавляет решение задачи №1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+const galleryRef = document.querySelector('.gallery');
 
-console.log(galleryItems);
+
+function createGalleryMarkap(item) {
+   
+    return item.map(({ preview, original, description }) => {
+       return `<div class="gallery__item">
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</div>`
+    }).join('')
+};
+
+const allGalleryMarkap = createGalleryMarkap(galleryItems);
+
+galleryRef.innerHTML =allGalleryMarkap
+
+new SimpleLightbox('.gallery a', { captions: true, captionsData: 'alt', captionDelay:250,});
