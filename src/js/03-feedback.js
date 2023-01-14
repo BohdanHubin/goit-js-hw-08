c441c3acd84c877c0b62e2688086f8f742f11ec2
BohdanHubin <111269462+BohdanHubin@users.noreply.github.com>
2023-01-14 19:20:50 +0200

    добавляет решение задачи №3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('[name="email"]');
+const message = document.querySelector('[name="message"]');
+
+form.addEventListener('input', throttle(onFormData, 500));
+form.addEventListener('submit', onSubmitForm);
+
+
+
+const formData = {
+    email: '',
+    message: '',};
+
+
+
+function onFormData (event) {
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function onSubmitForm (event) {
+    console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+    event.preventDefault();
+    event.currentTarget.reset();
+    localStorage.removeItem('feedback-form-state');
+}
+
+(function dataFromLocalStorage() {
+  const data = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (data) {
+    email.value = data.email;
+    message.value = data.message;
+  }
+})();
+
+
+
 
